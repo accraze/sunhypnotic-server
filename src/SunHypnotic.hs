356e207cc84daec47880dad8ad8f160eb719634d
accraze <accraze@gmail.com>
2016-02-20 09:03:05 -0800

    Added local MongoDB backend

--- src/SunHypnotic.hs
+++ src/SunHypnotic.hs
@@ -3,10 +3,18 @@
 module SunHypnotic where
 
 import Web.Scotty
+import Database.MongoDB
+import qualified Data.Text.Lazy as T
+import Control.Monad.IO.Class
 import SunHypnotic.Releases
+
+
+runQuery :: Pipe -> Query -> ActionM [Document]
+runQuery pipe query = access pipe master "sunhypnotic" (find query >>= rest) 
 
 serve = do
   putStrLn "Starting Server..."
+  pipe <- connect $ host "127.0.0.1"
   scotty 3000 $ do
     get "/releases" $ do
       json allReleases
@@ -16,3 +24,6 @@ serve = do
     post "/releases" $ do
       release <- jsonData :: ActionM Release
       json release
+    get "/mongo" $ do
+      res <- runQuery pipe (select [] "releases")
+      text $ T.pack $ show res
